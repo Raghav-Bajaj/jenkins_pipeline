afcb9fcacc43f53e5e96c9ae7e8d939aa9d55e5d
Raghav-Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2020-10-20 20:55:53 +0530

    Will  make Fuzz  and buss everytime

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -5,10 +5,8 @@ public class FizzBuzz {
     public String play(int number) {
 
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
-
-
-        return String.valueOf(number);
+        if (number % 2 == 0) return "Fizz";
+        
+                 return "Buzz";
     }
 }
